f506489f578ec55512d0b88ac7e47b47a5f1ceb4
Lena <me@lena.place>
2023-04-23 23:38:25 +0200

    make stuff work

--- src/main.rs
+++ src/main.rs
@@ -2,17 +2,20 @@ use ctru::prelude::*;
 use ctru::services::cfgu::Cfgu;
 use ctru::applets::swkbd::{Button, Swkbd, ValidInput, Filters, Features};
 use std::net::Ipv4Addr;
+//use thrussh::*;
+//use thrussh::server::{Auth, Session};
+//use thrussh_keys::*;
 
 fn main() {
-    ctru::init();
+    ctru::use_panic_handler();
     let apt = Apt::init().unwrap();
-    let hid = Hid::init().unwrap();
+    let mut hid = Hid::init().unwrap();
     let gfx = Gfx::init().unwrap();
     let soc = Soc::init().unwrap();
     let cfgu = Cfgu::init().unwrap();
-    let console = Console::init(gfx.top_screen.borrow_mut());
+    let _console = Console::init(gfx.top_screen.borrow_mut());
 
-    let address = soc.host_address();
+    //let address = soc.host_address();
 
     setup(cfgu, soc);
 
@@ -24,7 +27,7 @@ fn main() {
         let keys = hid.keys_held();
 
 
-        if keys.intersects(KeyPad::KEY_START) {
+        if keys.intersects(KeyPad::START) {
             println!("Exiting...");
             if status == AppStatus::Connected
             {
@@ -33,7 +36,7 @@ fn main() {
             break;   
         }
 
-        if (keys.intersects(KeyPad::KEY_A) && status == AppStatus::NotConnected)
+        if (keys.intersects(KeyPad::A) && status == AppStatus::NotConnected)
         {
             let mut keyboard = Swkbd::default();
             
@@ -43,19 +46,16 @@ fn main() {
 
             keyboard.set_validation(ValidInput::NotEmptyNotBlank, Filters::BACKSLASH);
 
-            // String used to store text received from the keyboard
-            let mut text = String::new();
-
             // Raise the software keyboard. You can perform different actions depending on which
             // software button the user pressed
-            match keyboard.get_utf8(&mut text) {
-                Ok(Button::Right) => 
+            match keyboard.get_string(32) {
+                Ok((text, Button::Right)) => 
                 {
                     println!("Connecting to {}...", text);
                     status = AppStatus::Connected;
                 }
-                Ok(Button::Left) => println!("Cancelled"),
-                Ok(Button::Middle) => println!("How did you even press this?"),
+                Ok((_, Button::Left)) => println!("Cancelled"),
+                Ok((_, Button::Middle)) => println!("How did you even press this?"),
                 Err(_) => println!("An error occurred."),
             }
         }
@@ -70,7 +70,7 @@ fn main() {
 fn setup(cfgu: Cfgu, soc: Soc) {
     println!("ctr-ssh-rs v0.1.0 by Lena");
     println!("https://github.com/adryzz/ctr-ssh-rs");
-    println!("IP: {}, running on {:?}", soc.host_address(), cfgu.get_model().unwrap());
+    println!("IP: {}, running on {:?}", soc.host_address(), cfgu.model().unwrap());
     println!("Press START to exit or A to connect to a server.");
 
     println!("\u{001b}[46;1m                \u{001b}[0m");
